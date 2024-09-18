94f90b153721829e35146cf5c6225a2e4c03e079
Joaquín Ballara <joaquinballara02@gmail.com>
2024-09-18 19:37:46 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import './App.css'
-import Card from './Card/Card.tsx'
+import Input from './Input/Input'
 
 function App() {
 
   return (
-    <div className='container'>
-      <Card>
-        <h2>Task 1</h2>
-        <p>Task 1 description</p>
-        <p>Assigned to: John Doe</p>
-        <p>Start date: 2021-01-01</p>
-        <p>Due date: 2021-01-31</p>
-      </Card>
-      <Card>
-        <h2>Task 2</h2>
-        <p>Task 2 description</p>
-        <p>Assigned to: Jane Doe</p>
-        <p>Start date: 2021-02-01</p>
-        <p>Due date: 2021-02-28</p>
-      </Card>
-      <Card>
-        <h2>Task 3</h2>
-        <p>Task 3 description</p>
-        <p>Assigned to: John Doe</p>
-        <p>Start date: 2021-03-01</p>
-        <p>Due date: 2021-03-31</p>
-      </Card>
-    </div>
+    <>
+      <Input />
+    </>
   )
 }
 
