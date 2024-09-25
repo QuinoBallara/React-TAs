1485f3fb44e918bfe1f9f67ac4c4531b22c502a7
Joaquín Ballara <joaquinballara02@gmail.com>
2024-09-25 19:01:29 -0300

    fin ta10

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
+import React, { useEffect, useState } from 'react'
 
 function App() {
 
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setData(data))
+    console.log(data)
+  }, [])
+
+
   return (
-    <>
-      <p>hello world</p>
-    </>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {data && data.map((user: any) => (
+          <li key={user.id}><span>{user.name}</span>: {user.email}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
